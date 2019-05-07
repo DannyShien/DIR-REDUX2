04e37c2f7d44b5d4128c5f5c475862ac8684a19c
Stanley Chen <dannyshien@gmail.com>
2019-05-07 09:50:47 -0400

    add skeleton of pokemone deck

---
+++ pokedux/src/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+
+// ===============================================================================
+// ===== ===== STATE ===== =====
+import initialState from './Base.json'
+console.log(initialState);
+
+// the state is an object,with a cards property, which is an array of objects
+
+
+// ===============================================================================
+// ===== ===== ACTION + ACTION CREATORS ===== =====
+const ACTION_CATCH = 'catch';
+
+function catchCard (id)  {
+    return {
+        type: ACTION_CATCH,
+        payload: {
+            id
+        }
+    }
+}
+
+
+
+
+// ===============================================================================
+// ===== ===== REDUCER ===== =====
+// Deals with STATE and ACTION
+function cards (state=initialState, action={type: ''}) {
+
+    switch(action.type) {
+        case ACTION_CATCH: 
+            // find the card, set it to 'caught'
+        break;
+        default: 
+            return state;
+        break;
+    }
+}
+
+
+
+// ===============================================================================
+// ===== ===== STORE ===== =====
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
